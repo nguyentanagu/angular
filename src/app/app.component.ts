98928b5127d089947ebbcaad9c8e947d9c81f321
nguyentan <nguyentan0842010534@gmail.com>
2025-02-28 01:38:24 +0700

    new angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderLayoutComponent } from "./shared/header-layout/header-layout.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, HeaderLayoutComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular';
+  nameBtn: string = 'Click Me';  // Khai báo biến nameBtn
+  clickMessage: string = '';     // Khai báo biến clickMessage
+
+  handleClickMe() {
+    this.clickMessage = 'Button Clicked!';  // Thêm phương thức handleClickMe
+  }
 }
+
